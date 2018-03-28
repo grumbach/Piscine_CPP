12b14ec12c383c253f7a56f1da3cdbdffdc783b1
Hugo MASSONNET <hmassonn@e2r11p9.42.fr>
2018-03-28 13:41:21 +0200

    [ADD] d04 ex01

---
+++ d04/ex01/RadScorpion.hpp
@@ -0,0 +1,18 @@
+#ifndef RADSCORPION_HPP
+# define RADSCORPION_HPP
+
+#include "Enemy.hpp"
+
+class RadScorpion : public Enemy
+{
+
+public:
+    RadScorpion(RadScorpion const & src);
+    RadScorpion(void);
+    ~RadScorpion(void);
+
+    RadScorpion & operator=( RadScorpion const & rhs );
+
+};
+
+#endif
