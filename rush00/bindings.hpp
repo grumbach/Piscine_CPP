d5fa2c2396977e4562aa2e6c709bca3d6bbffc7e
Raphael BADIA <07fortune@gmail.com>
2018-03-29 18:45:07 +0200

    [add] keybinding

---
+++ rush00/bindings.hpp
@@ -0,0 +1,11 @@
+#ifndef BINDINGS_HPP
+# define BINDINGS_HPP
+
+# define KEY_NONE -1
+# define KEY_ARROW_UP 3
+# define KEY_ARROW_LEFT 4
+# define KEY_ARROW_RIGHT 5
+# define KEY_ARROW_DOWN 2
+# define KEY_SPACE 32
+
+#endif
