b208f86dc32c4b5382f867ba64839504314571a4
grumbach <agrumbac@student.42.fr>
2018-03-30 11:42:43 +0200

    [FIX] various small fixes

--- d04/ex03/Ice.cpp
+++ d04/ex03/Ice.cpp
@@ -6,7 +6,7 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/27 17:31:17 by agrumbac          #+#    #+#             */
-/*   Updated: 2018/03/27 19:28:52 by agrumbac         ###   ########.fr       */
+/*   Updated: 2018/03/30 11:40:46 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,5 @@ AMateria *		Ice::clone( void ) const
 
 void			Ice::use( ICharacter & target )
 {
-	AMateria::use(target);
 	std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
 }
